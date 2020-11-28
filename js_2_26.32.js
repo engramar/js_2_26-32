2298a609545d56c138ca8dc635c6e68d8bb02a9e
Engramar Bollas <engramar.bollas@gmail.com>
2020-11-28 21:09:56 +1100

    Update js_2_26.32.js

--- js_2_26.32.js
+++ js_2_26.32.js
@@ -33,5 +33,37 @@ const makeServerRequest = new Promise((resolve, reject) => {
 });
 
 #Handle a Fulfilled Promise with then
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+
+  makeServerRequest.then(result => {
+    console.log (result) 
+});
+});
 
 #Handle a Rejected Promise with catch
+const makeServerRequest = new Promise((resolve, reject) => {
+  // responseFromServer is set to false to represent an unsuccessful response from a server
+  let responseFromServer = false;
+    
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {  
+    reject("Data not received");
+  }
+});
+
+makeServerRequest.then(result => {
+  console.log(result);
+});
+
+makeServerRequest.catch(error => {
+  console.log(error);
+});
